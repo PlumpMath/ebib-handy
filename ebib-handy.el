4b2e3e58fdae1ecf213e104978eb1bde186ebeba
Feng Shu <tumashu@163.com>
2016-05-10 06:55:43 +0800

    Fix type

--- ebib-handy.el
+++ ebib-handy.el
@@ -16,12 +16,12 @@
 ;; modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 3
 ;; of the License, or (at your option) any later version.
-;; ;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; ;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
@@ -37,7 +37,6 @@
 ;; ** Install
 ;; 1. Config melpa: http://melpa.org/#/getting-started
 ;; 2. M-x package-install RET ebib-handy RET
-;; 3. Add code to your emacs config file:（for example: ~/.emacs）：
 
 ;; ** Configure
 ;; #+BEGIN_EXAMPLE
